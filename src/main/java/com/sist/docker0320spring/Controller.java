7d133e5228f09fa3b4eff7ce4b7e5604a06724f1
ted19horse <153588307+ted19horse@users.noreply.github.com>
2025-03-25 18:47:24 +0900

    build test 5 connect DB

--- src/main/java/com/sist/docker0320spring/Controller.java
+++ src/main/java/com/sist/docker0320spring/Controller.java
@@ -1,12 +1,22 @@
 package com.sist.docker0320spring;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class DockerController {
   @GetMapping("/")
   public String index() {
     return "Hello Docker!?!?!!??!?!?!";
+  }
+
+  @GetMapping("/sample/all")
+  public Map<String, Object> sampleAll() {
+    Map<String, Object> map = new HashMap<>();
+    return map;
   }
 }
