4562583c7ae7a0e2c52413d674ef68a15a6c92a1
ted19horse <153588307+ted19horse@users.noreply.github.com>
2025-03-25 17:54:30 +0900

    build test

---
+++ src/main/java/com/sist/docker0320spring/DockerController.java
@@ -0,0 +1,12 @@
+package com.sist.docker0320spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+  @GetMapping("/")
+  public String index() {
+    return "Hello Docker!";
+  }
+}
