f47df78383d52897d10b92005636329b829500a0
ted19horse <153588307+ted19horse@users.noreply.github.com>
2025-03-25 18:06:15 +0900

    build test3

--- src/main/java/com/sist/docker0320spring/DockerController.java
+++ src/main/java/com/sist/docker0320spring/DockerController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerController {
   @GetMapping("/")
   public String index() {
-    return "Hello Docker!!!!!!!!!!!!!!!!!!";
+    return "Hello Docker????????????";
   }
 }
